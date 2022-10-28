4d9457f49270ae7e6a1447098fa064b09569b567
hphat204 <pphat620@gmail.com>
2022-10-28 10:18:29 +0700

    fixed bug

--- js/index.js
+++ js/index.js
@@ -1,49 +1,63 @@
 const infoText = document.querySelectorAll(".infoText");
-let inputValue = document.querySelector(".inputValue");
+const inputValue = document.querySelector(".inputValue");
 const searchBtn = document.querySelector(".search-btn");
 
-const apiKey = process.env.API_KEY;
-
+const apiKey = "at_8L1PYQbGQim9CFqSsWifniWWNS5pR";
+var map;
 function initializedMap() {
-  map = L.map("map").setView([0, 0], 13);
+  map = L.map("map").setView([51.505, -0.09], 13);
   L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
   }).addTo(map);
-  L.marker([0, 0]).addTo(map);
+  L.marker([51.5, -0.09]).addTo(map);
 }
 
 initializedMap();
 async function getUserIp(ipOrDomain) {
-  const reponse = await fetch(
-    `https://geo.ipify.org/api/v2/country,city?apiKey=at_8L1PYQbGQim9CFqSsWifniWWNS5pR&ipAddress=${
-      ipOrDomain ? ipOrDomain : ""
-    }&domain=${ipOrDomain ? ipOrDomain : ""}`
-  );
-  const data = await reponse.json();
-  return data;
+  try {
+    const reponse = await fetch(
+      `https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}&ipAddress=${ipOrDomain ? ipOrDomain : ""}&domain=${
+        ipOrDomain ? ipOrDomain : ""
+      }`
+    );
+    const data = await reponse.json();
+    return data;
+  } catch (error) {
+    throw error;
+  }
 }
-
-setUserData(getUserIp());
 
 async function setUserData(userData) {
-  const {
-    ip,
-    location: { country, timezone, lat, lng },
-    isp,
-  } = await userData;
-
-  infoText.forEach((text) => {
-    if (text.classList.contains("ipAddress")) document.querySelector(".ipAddress").textContent = ip;
-    if (text.classList.contains("country")) document.querySelector(".country").textContent = country;
-    if (text.classList.contains("timezone")) document.querySelector(".timezone").textContent = timezone;
-    if (text.classList.contains("isp")) document.querySelector(".isp").textContent = isp;
-  });
-  map.setView([lat, lng], 13);
-  L.marker([lat, lng]).addTo(map);
+  try {
+    const {
+      ip,
+      location: { country, timezone, lat, lng },
+      isp,
+    } = userData;
+    infoText.forEach((text) => {
+      if (text.classList.contains("ipAddress")) document.querySelector(".ipAddress").textContent = ip;
+      if (text.classList.contains("country")) document.querySelector(".country").textContent = country;
+      if (text.classList.contains("timezone")) document.querySelector(".timezone").textContent = timezone;
+      if (text.classList.contains("isp")) document.querySelector(".isp").textContent = isp;
+    });
+    map.flyTo([lat, lng], 11);
+    L.marker([lat, lng]).addTo(map);
+  } catch (error) {
+    throw error;
+  }
 }
 
-searchBtn.addEventListener("click", async () => {
-  if (!inputValue.value) return;
-  const data = getUserIp(inputValue.value);
-  setUserData(data);
+searchBtn.addEventListener("click", async (e) => {
+  try {
+    e.preventDefault();
+    inputValue.classList.remove("wrong-search");
+    if (!inputValue.value) return;
+    const data = await getUserIp(inputValue.value);
+    if (data.code === 422) inputValue.classList.add("wrong-search");
+    setUserData(data);
+    inputValue.value = "";
+  } catch (error) {
+    console.log(error);
+  }
 });
